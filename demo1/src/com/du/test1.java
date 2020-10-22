f65599277bf4adbb368efdd726d80d5f2ade64ed
huanhuan8888 <1274050090@qq.com>
2020-10-22 15:05:26 +0800

    master分支的修改

--- demo1/src/com/du/test1.java
+++ demo1/src/com/du/test1.java
@@ -2,13 +2,6 @@ package com.du;
 
 public class test1 {
     public static void main(String[] args) {
-            System.out.println("刘威傻子");
-            System.out.println("修改了第一次的数据");
-        System.out.println("加了一个补丁");
-        System.out.println("修改的一条文件");
-        System.out.println("再拉一条");
-        System.out.println("xiugaifenzhi1111test");
-        System.out.println("111111");
-        System.out.println("masterxiugaile");
+        System.out.println("mater分支的修改222222");
     }
 }
