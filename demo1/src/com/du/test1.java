f598e3c4f0716b50e81236ef50686f523aa650be
huanhuan8888 <1274050090@qq.com>
2020-10-22 08:11:17 +0800

    第二次提交数据

--- demo1/src/com/du/test1.java
+++ demo1/src/com/du/test1.java
@@ -4,5 +4,6 @@ public class test1 {
     public static void main(String[] args) {
         System.out.println("刘威傻子");
         System.out.println("cccccc");
+        System.out.println("sdada");
     }
 }
