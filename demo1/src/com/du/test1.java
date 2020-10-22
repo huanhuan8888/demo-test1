9f399e9bcb844980091efc29b162b8c5e729dc99
huanhuan8888 <1274050090@qq.com>
2020-10-22 08:24:38 +0800

    第五次提交数据

--- demo1/src/com/du/test1.java
+++ demo1/src/com/du/test1.java
@@ -2,8 +2,8 @@ package com.du;
 
 public class test1 {
     public static void main(String[] args) {
-        System.out.println("刘威傻子");
-        System.out.println("修改了第一次的数据");
-
+            System.out.println("刘威傻子");
+            System.out.println("修改了第一次的数据");
+        System.out.println("加了一个补丁");
     }
 }
